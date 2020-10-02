7ab110fee3203acb523a79917e44d09e93b0cdc5
AHaustov <anton.haust@gmail.com>
2020-10-02 13:13:31 +0200

    day19

---
+++ src/day19/ClassWork19.java
@@ -0,0 +1,13 @@
+package day19;
+
+public class ClassWork19 {
+    public static void main(String[] args) {
+        System.out.println(homework0_1());
+    }
+
+    private static double[] homework0_1() {
+        double[] output;
+        output= new double[10];
+        return output;
+    }
+}
