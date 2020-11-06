ce4fcf89ba8ebb30ff017be12b1a3b9c75d92beb
AHaustov <anton.haust@gmail.com>
2020-11-06 22:21:24 +0100

    updated

--- src/linuxBeginner/sleeper.sh
+++ src/linuxBeginner/sleeper.sh
@@ -3,13 +3,14 @@ for run in {1..100}
 do
 date +%H:%M:%S 
  echo $run
- ps -ef | head -2
- #awk -F = {'print$2'} /etc/os-release | head  -1
- awk -F = '$1=="NAME" {print$2}' /etc/os-release 
- #sleep 5 
+ ps -ef | tail +2 | wc -l
+  #sleep 5 
  sleep 1
 done 
 
+cat cpuinfo | grep "model name"| head -1 > ./cpu
 
+#awk -F = {'print$2'} /etc/os-release | head  -1
+ awk -F = '$1=="NAME" {print$2}' /etc/os-release >> ./cpu
 
 touch ./{1..100}.txt
