1c5c7ad0102a0a290248d1b964b577f3ccc44e67
AHaustov <anton.haust@gmail.com>
2020-11-06 22:01:23 +0100

    updated

--- src/linuxBeginner/sleeper.sh
+++ src/linuxBeginner/sleeper.sh
@@ -4,7 +4,9 @@ do
 date +%H:%M:%S 
  echo $run
  ps -ef | head -2
- awk -F = {'print$2'} /etc/os-release | head  -1
+ #awk -F = {'print$2'} /etc/os-release | head  -1
+ awk -F = '$1=="NAME" {print$2}' /etc/os-release 
+"Ubuntu"
  #sleep 5 
  sleep 1
 done 
